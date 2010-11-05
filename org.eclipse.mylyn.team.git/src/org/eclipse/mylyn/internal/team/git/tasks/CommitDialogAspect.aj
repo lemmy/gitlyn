2397f6bffc87f09b8c87519264847a2373f2b436
Markus Alexander Kuppe <bugs.eclipse.org@lemmster.de>
2010-11-05 14:04:23 +0100

    Use AspectJ to set the commit message

---
+++ org.eclipse.mylyn.team.git/src/org/eclipse/mylyn/internal/team/git/tasks/CommitDialogAspect.aj
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Markus Alexander Kuppe and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *     Markus Alexander Kuppe <egit@lemmster.de> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.internal.team.git.tasks;
+
+import org.eclipse.egit.ui.internal.dialogs.CommitDialog;
+import org.eclipse.mylyn.internal.team.ui.FocusedTeamUiPlugin;
+import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.mylyn.tasks.core.ITaskActivityManager;
+import org.eclipse.mylyn.tasks.ui.TasksUi;
+
+@SuppressWarnings("restriction")
+public aspect CommitDialogAspect {
+	/**
+	 * Inject the commit message into the member prior to
+	 * org.eclipse.egit.ui.internal
+	 * .dialogs.CommitDialog.createDialogArea(Composite) invocation
+	 */
+	pointcut getCommitMessage(CommitDialog commitDialog) : 
+		this(commitDialog) 
+		&& within(CommitDialog) 
+		&& get(private String CommitDialog.commitMessage);
+
+	before(CommitDialog cd): getCommitMessage(cd) {
+		cd.setCommitMessage(getCommitMessageFromCurrentTask());
+	}
+
+	/**
+	 * @return The emptry String or a commit message from the current active
+	 *         task
+	 */
+	private static String getCommitMessageFromCurrentTask() {
+		try {
+			// get active task
+			ITaskActivityManager tam = TasksUi.getTaskActivityManager();
+			ITask task = tam.getActiveTask();
+			if(task == null)
+				return "";
+
+			// get the commit dialog template
+			String template = FocusedTeamUiPlugin.getDefault().getPreferenceStore().getString(
+						FocusedTeamUiPlugin.COMMIT_TEMPLATE);
+			return FocusedTeamUiPlugin.getDefault().getCommitTemplateManager().generateComment(task, template);
+		} catch(Exception e) {
+			return "";
+		}
+	}
+}
--- org.eclipse.mylyn.team.git/src/org/eclipse/mylyn/internal/team/git/tasks/TaskActivationListener.java
+++ org.eclipse.mylyn.team.git/src/org/eclipse/mylyn/internal/team/git/tasks/TaskActivationListener.java
@@ -6,55 +6,39 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * Contributors:
  *     Chris Aniszczyk <caniszczyk@gmail.com> - initial API and implementation
+ *     Markus Alexander Kuppe <egit@lemmster.de>
  *******************************************************************************/
 package org.eclipse.mylyn.internal.team.git.tasks;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.egit.core.Activator;
-import org.eclipse.egit.core.op.BranchOperation;
-import org.eclipse.jface.window.Window;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.core.ITaskActivationListener;
-import org.eclipse.ui.PlatformUI;
 
 public class TaskActivationListener implements ITaskActivationListener {
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.mylyn.tasks.core.ITaskActivationListener#preTaskActivated(org.eclipse.mylyn.tasks.core.ITask)
+	 */
 	public void preTaskActivated(ITask task) {
-		// TODO if there's a context, should we browse it to deduce which repo to choose?
-		String branch = Constants.R_HEADS + task.getTaskId();
-		RepositoryAndBranchSelectionDialog dialog = new RepositoryAndBranchSelectionDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), branch);
-		if (dialog.open() == Window.OK) {
-			try {
-				BranchOperation operation = new BranchOperation(dialog.getRepository(), dialog.getBranch());
-				operation.execute(null);
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
-		}
-		
+		//nop
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.mylyn.tasks.core.ITaskActivationListener#preTaskDeactivated(org.eclipse.mylyn.tasks.core.ITask)
+	 */
 	public void preTaskDeactivated(ITask task) {
-		// TODO how do we handle the case of switching to a new task from an old task
+		//nop
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.mylyn.tasks.core.ITaskActivationListener#taskActivated(org.eclipse.mylyn.tasks.core.ITask)
+	 */
 	public void taskActivated(ITask task) {
-		// do nothing
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.mylyn.tasks.core.ITaskActivationListener#taskDeactivated(org.eclipse.mylyn.tasks.core.ITask)
+	 */
 	public void taskDeactivated(ITask task) {
-		// FIXME hack, we should detect which repository to switch to master
-		// we should base this off the task context imho... we should be able to guess based on the projects in the context
-		// if we get a conflict... this may be a bit more complicated... but how common would this be?
-		Repository repository = Activator.getDefault().getRepositoryCache().getAllRepositories()[0];
-		try {
-			BranchOperation operation = new BranchOperation(repository, Constants.MASTER);
-			operation.execute(null);
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
+		//nop
 	}
-
 }
